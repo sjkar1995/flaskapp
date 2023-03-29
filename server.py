be02e6af4855cace11e9677873459d996e44cad9
satyajeet <satya126812@gmail.com>
2023-03-29 08:09:11 +0530

    firstcommit

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+@app.route('/home')
+def index():
+    return render_template('home.html',username="satya")
+@app.route('/submit-form',methods=['POST', 'GET'])
+def formdata():
+    uname = request.form['name']
+    email = request.form['email']
+    return render_template('form.html',gname=uname,gemail=email)
